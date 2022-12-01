ce6ecb402cfbf531c2527efd9457e47e90a1f969
ErnieWhite <ernie.white.68@gmail.com>
2022-12-01 08:17:06 -0600

    Added second part of day 1

--- day_1_1/main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    with open('input.txt', 'r') as iFile:
-        reader = iFile.readlines()
-        for line in reader:
-            print(line)
-
-if __name__ == '__main__':
-    main()
-
